a90ae2673b220487e1ec10296fba2ee0d533ef05
Justin Littman <justinlittman@stanford.edu>
2025-04-08 08:50:54 -0400

    Adds more Dataworks schema validation.

--- spec/services/dataworks_validator_spec.rb
+++ spec/services/dataworks_validator_spec.rb
@@ -5,10 +5,64 @@ require 'rails_helper'
 RSpec.describe DataworksValidator do
   subject(:validator) { described_class.new(metadata:) }
 
+  context 'when required metadata is valid' do
+    let(:metadata) do
+      {
+        creators: [
+          { name: 'A. Researcher' }
+        ],
+        titles: [{ title: 'My title' }]
+      }
+    end
+
+    it 'returns true for valid?' do
+      expect(validator.valid?).to be true
+    end
+
+    it 'returns no errors' do
+      expect(validator.errors).to be_empty
+    end
+  end
+
   context 'when metadata is valid' do
     let(:metadata) do
       {
-        titles: [{ title: 'PRIME India' }]
+        doi: '10.1234/5678',
+        creators: [
+          { name: 'A. Researcher' },
+          {
+            name: 'B. Researcher', name_type: 'Personal', given_name: 'B.', family_name: 'Researcher',
+            name_identifiers: [
+              {
+                name_identifier: 'https://orcid.org/0000-0001-2345-6789', name_identifier_scheme: 'ORCID',
+                scheme_uri: 'https://orcid.org/'
+              }
+            ],
+            affiliation: [
+              { name: 'My institution', affiliation_identifier: 'https://ror.org/00f54p054', affiliation_identifier_scheme: 'ROR' }
+            ]
+          },
+          { name: 'A. Organization' },
+          {
+            name: 'B. Organization', name_type: 'Organizational',
+            name_identifiers: [{ name_identifier: 'https://ror.org/00f54p054' }],
+            affiliation: [{ name: 'B. Parent Organization' }]
+          }
+        ],
+        titles: [
+          { title: 'My title' },
+          { title: 'My subtitle', title_type: 'Subtitle' }
+        ],
+        descriptions: [
+          { description: 'My description' },
+          { description: 'My abstract', description_type: 'Abstract' }
+        ],
+        dates: [
+          { date: '2023-01-01' },
+          { date: '2023-01-02T19:20:30+01:00' },
+          { date: '2023-01-03', date_type: 'Updated' }
+        ],
+        version: '1.0'
       }
     end
 
@@ -37,7 +91,8 @@ RSpec.describe DataworksValidator do
       expect(validator.errors).to eq(
         [
           'array size at `/titles` is less than: 1',
-          'object property at `/another_field` is a disallowed additional property'
+          'object property at `/another_field` is a disallowed additional property',
+          'object at root is missing required properties: creators'
         ]
       )
     end
