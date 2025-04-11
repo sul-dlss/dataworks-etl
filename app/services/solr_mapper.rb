7224a852227f65d1c74809dc0f902eb77bc30a9b
Huda Khan <hudakhan@stanford.edu>
2025-04-11 17:44:54 -0600

    adding solr mapping

--- app/services/solr_mapper.rb
+++ app/services/solr_mapper.rb
@@ -17,12 +17,58 @@ class SolrMapper
   def call
     {
       id: dataset_record_id,
-      dataset_record_set_id: dataset_record_set_id,
-      title: metadata.dig(:titles, 0, :title)
-    }
+      dataset_record_set_id_ss: dataset_record_set_id,
+      access_ssi: metadata['access'],
+      provider_ssi: metadata['provider'],
+      provider_identifier_ssim: metadata['identifiers'].pluck('identifier'),
+      creators_struct_ss: metadata['creators'].to_json
+    }.merge(map_titles(metadata['titles']))
   end
 
   private
 
   attr_reader :metadata, :dataset_record_id, :dataset_record_set_id
+
+  # Given titles from metadata, return field value based on title type
+  def title_fields(title_type, titles)
+    titles.select { |title_obj| title_obj.key?('title_type') && title_obj['title_type'] == title_type }.pluck('title')
+  end
+
+  def map_titles(titles_metadata)
+    titles = titles_metadata.reject { |title_obj| title_obj.key?('title_type') }.pluck('title')
+    subtitles = title_fields('Subtitle', titles_metadata)
+    alternative_titles = title_fields('AlternativeTitle', titles_metadata)
+    translated_titles =  title_fields('TranslatedTitle', titles_metadata)
+    other_titles = title_fields('Other', titles_metadata)
+
+    titles = assign_titles(titles, subtitles, alternative_titles, translated_titles, other_titles)
+
+    map_typed_title_fields(titles, subtitles, alternative_titles, translated_titles, other_titles)
+  end
+
+  def assign_titles(titles, subtitles, alternative_titles, translated_titles, other_titles)
+    # One of the title elements is required, but we will use one of the other titles when present
+    return titles unless titles.empty?
+
+    if !alternative_titles.empty?
+      alternative_titles
+    elsif !subtitles.empty?
+      subtitles
+    elsif !other_titles.empty?
+      other_titles
+    else
+      translated_titles
+    end
+  end
+
+  def map_typed_title_fields(titles, subtitles, alternative_titles, translated_titles, other_titles)
+    mapped_titles = {
+      title_tsim: titles
+    }
+    mapped_titles[:subtitle_tsim] = subtitles unless subtitles.empty?
+    mapped_titles[:alternative_title_tsim] = alternative_titles unless alternative_titles.empty?
+    mapped_titles[:translate_title_tsim] = translated_titles unless translated_titles.empty?
+    mapped_titles[:other_title_tsim] = other_titles unless other_titles.empty?
+    mapped_titles
+  end
 end
--- spec/services/solr_mapper_spec.rb
+++ spec/services/solr_mapper_spec.rb
@@ -7,19 +7,151 @@ RSpec.describe SolrMapper do
 
   let(:metadata) do
     {
-      titles: [{ title: 'PRIME India' }]
+      creators: [
+        { name: 'A. Researcher' },
+        {
+          name: 'B. Researcher', name_type: 'Personal', given_name: 'B.', family_name: 'Researcher',
+          name_identifiers: [
+            {
+              name_identifier: 'https://orcid.org/0000-0001-2345-6789', name_identifier_scheme: 'ORCID'
+            }
+          ],
+          affiliation: [
+            { name: 'My institution', affiliation_identifier: 'https://ror.org/00f54p054', affiliation_identifier_scheme: 'ROR' }
+          ]
+        },
+        { name: 'A. Organization' },
+        {
+          name: 'B. Organization', name_type: 'Organizational',
+          name_identifiers: [{ name_identifier: 'https://ror.org/00f54p054' }],
+          affiliation: [{ name: 'B. Parent Organization' }]
+        }
+      ],
+      titles: [
+        { title: 'My title' },
+        { title: 'My subtitle', title_type: 'Subtitle' },
+        { title: 'My alt title', title_type: 'AlternativeTitle' },
+        { title: 'My translated title', title_type: 'TranslatedTitle' },
+        { title: 'My other title', title_type: 'Other' }
+      ],
+      publisher: {
+        name: 'My publisher',
+        schema_uri: 'https://ror.org/',
+        publisher_identifier: 'https://ror.org/00f54p054', publisher_identifier_scheme: 'ROR'
+      },
+      publication_year: '2023',
+      subjects: [
+        { subject: 'My subject' },
+        {
+          subject: 'My subject 2',
+          subject_scheme: 'Library of Congress Subject Headings (LCSH)',
+          value_uri: 'https://id.loc.gov/authorities/subjects/sh85026447'
+        }
+      ],
+      contributors: [
+        { name: 'A. Contributor' },
+        {
+          name: 'B. Contributor', name_type: 'Personal', given_name: 'B.', family_name: 'Contributor',
+          name_identifiers: [
+            {
+              name_identifier: 'https://orcid.org/0000-0001-2345-6789', name_identifier_scheme: 'ORCID'
+            }
+          ],
+          affiliation: [
+            { name: 'My institution', affiliation_identifier: 'https://ror.org/00f54p054', affiliation_identifier_scheme: 'ROR' }
+          ],
+          contributor_type: 'DataCollector'
+        },
+        { name: 'A. Organization' },
+        {
+          name: 'B. Organization', name_type: 'Organizational',
+          name_identifiers: [{ name_identifier: 'https://ror.org/00f54p054' }],
+          affiliation: [{ name: 'B. Parent Organization' }],
+          contributor_type: 'RegistrationAgency'
+        }
+      ],
+      descriptions: [
+        { description: 'My description' },
+        { description: 'My abstract', description_type: 'Abstract' }
+      ],
+      dates: [
+        { date: '2023-01-01' },
+        { date: '2023-01-02T19:20:30+01:00' },
+        { date: '2023-01-03', date_type: 'Updated' },
+        { date: '2022-01-01/2022-12-31' },
+        { date: '2022-11-25' },
+        { date: '1973' },
+        { date: '2008-06-08T00:00:00Z/2008-07-04T00:00:00Z' },
+        { date: '2006-12-20T00:00:00.000Z/2023-10-06T23:59:59.999Z' },
+        { date: '0600-01-01/1800-01-01' }
+      ],
+      language: 'en',
+      identifiers: [
+        { identifier: 'stanfordphs.prime_india:016c:v0_1', identifier_type: 'RedivisReference' },
+        { identifier: '10.1234/5678', identifier_type: 'DOI' }
+      ],
+      related_identifiers: [
+        { related_identifier: '10.1234/5678' },
+        {
+          related_identifier: '10.2345/6789',
+          relation_type: 'IsCitedBy',
+          resource_type_general: 'JournalArticle',
+          related_identifier_type: 'DOI'
+        }
+      ],
+      sizes: ['1.2 MB', '3 pages'],
+      formats: ['application/pdf', '.pdf'],
+      version: '1.0',
+      rights_list: [
+        { rights: 'My rights' },
+        {
+          rights: 'Creative Commons Attribution 4.0 International',
+          rights_uri: 'https://creativecommons.org/licenses/by/4.0/',
+          rights_identifier: 'CC-BY-4.0',
+          rights_identifier_scheme: 'SPDX'
+        },
+        {
+          rights_uri: 'https://creativecommons.org/licenses/by/4.0/'
+        }
+      ],
+      funding_references: [
+        { funder_name: 'My funder' },
+        {
+          funder_name: 'My other funder',
+          funder_identifier: 'https://ror.org/00f54p054',
+          funder_identifier_type: 'ROR',
+          award_number: '123456',
+          award_uri: 'https://doi.org/10.1234/5678',
+          award_title: 'My award title'
+        }
+      ],
+      url: 'https://example.com/my-dataset',
+      variables: ['variable 1', 'variable 2'],
+      data_use_statement: 'My data use statement',
+      access: 'Restricted',
+      provider: 'Zenodo'
     }
   end
 
+  # rubocop:disable Layout/LineLength
   describe '#call' do
     it 'maps to Solr metadata' do
       expect(solr_mapper.call).to eq(
         {
           id: 123,
-          dataset_record_set_id: 456,
-          title: 'PRIME India'
+          dataset_record_set_id_ss: 456,
+          title_tsim: ['My title'],
+          subtitle_tsim: ['My subtitle'],
+          alternative_title_tsim: ['My alt title'],
+          translate_title_tsim: ['My translated title'],
+          other_title_tsim: ['My other title'],
+          access_ssi: 'Restricted',
+          provider_ssi: 'Zenodo',
+          provider_identifier_ssim: ['stanfordphs.prime_india:016c:v0_1', '10.1234/5678'],
+          creators_struct_ss: '[{"name":"A. Researcher"},{"name":"B. Researcher","name_type":"Personal","given_name":"B.","family_name":"Researcher","name_identifiers":[{"name_identifier":"https://orcid.org/0000-0001-2345-6789","name_identifier_scheme":"ORCID"}],"affiliation":[{"name":"My institution","affiliation_identifier":"https://ror.org/00f54p054","affiliation_identifier_scheme":"ROR"}]},{"name":"A. Organization"},{"name":"B. Organization","name_type":"Organizational","name_identifiers":[{"name_identifier":"https://ror.org/00f54p054"}],"affiliation":[{"name":"B. Parent Organization"}]}]'
         }
       )
     end
   end
+  # rubocop:enable Layout/LineLength
 end
