507fced3434a170134fda591076fde594f015d20
Nick Budak <budak@stanford.edu>
2025-04-21 13:07:20 -0700

    Update the SW extractor tests

--- spec/services/extractors/searchworks_spec.rb
+++ spec/services/extractors/searchworks_spec.rb
@@ -3,25 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Extractors::Searchworks do
-  let(:example_marc) do
-    {
-      'fields' => [
-        {
-          '856' => {
-            'subfields' => [
-              { 'u' => 'http://doi.org/10.3886/ICPSR37620.v1' }
-            ]
-          }
-        }
-      ]
-    }
-  end
-
   let(:example_doc) do
     {
       'id' => '123',
       'last_updated' => '2023-01-01T00:00:00Z',
-      'marc_json_struct' => example_marc.to_json
+      'url_fulltext' => ['http://doi.org/10.3886/ICPSR37620.v1']
     }
   end
 
@@ -31,7 +17,7 @@ RSpec.describe Extractors::Searchworks do
     extractor = described_class.new(list_args:, client:, extra_dataset_ids: [])
     params = extractor.send(:list_args)[:params]
     expect(params).to include(q: 'test')
-    expect(params).to include(fl: 'id,last_updated,marc_json_struct')
+    expect(params[:fl]).to include('title_display')
   end
 
   it 'maps solr docs into dataset records' do
@@ -42,45 +28,6 @@ RSpec.describe Extractors::Searchworks do
     record = DatasetRecord.last
     expect(record.dataset_id).to eq('123')
     expect(record.modified_token).to eq('2023-01-01T00:00:00Z')
-    expect(record.doi).to eq('http://doi.org/10.3886/ICPSR37620.v1')
-    expect(record.source).to eq(example_marc)
-  end
-
-  context 'when there is no doi in the marc' do
-    let(:example_marc) do
-      {
-        'fields' => [
-          {
-            '856' => {
-              'subfields' => []
-            }
-          }
-        ]
-      }
-    end
-
-    it 'does not error and sets doi to nil' do
-      list_args = { params: { q: 'test' } }
-      client = instance_double(Clients::Solr, list: [example_doc], dataset: {})
-      extractor = described_class.new(list_args:, client:, extra_dataset_ids: [])
-      extractor.call
-      record = DatasetRecord.last
-      expect(record.doi).to be_nil
-    end
-  end
-
-  context 'when there is an extra (hardcoded) dataset id' do
-    it 'retrieves the extra dataset' do
-      list_args = { params: { q: 'test' } }
-      client = instance_double(Clients::Solr, list: [])
-      extractor = described_class.new(list_args:, client:, extra_dataset_ids: ['123'])
-      allow(client).to receive(:dataset).with(id: '123').and_return(example_doc)
-      extractor.call
-      record = DatasetRecord.last
-      expect(record.dataset_id).to eq('123')
-      expect(record.modified_token).to eq('2023-01-01T00:00:00Z')
-      expect(record.doi).to eq('http://doi.org/10.3886/ICPSR37620.v1')
-      expect(record.source).to eq(example_marc)
-    end
+    expect(record.doi).to eq('10.3886/ICPSR37620.v1')
   end
 end
