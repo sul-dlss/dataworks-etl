3faed5e03c94988901cd8178c0ca5b1d7b088164
Huda Khan <hudakhan@stanford.edu>
2025-10-03 10:56:01 -0600

    setting up cleanup

--- app/services/metadata_cleaner.rb
+++ app/services/metadata_cleaner.rb
@@ -3,7 +3,7 @@
 # Performs transformation from source metadata to Solr documents for a single dataset
 class MetadataCleaner
   # Fields for cleanup
-  FIELDS = %w[creators_ssim contributors_ssim funders_ssim subjects_ssim].freeze
+  FIELDS = %w[creators_ssim contributors_ssim funders_ssim subjects_ssim publisher_ssi].freeze
 
   def self.call(...)
     new(...).call
@@ -20,16 +20,19 @@ class MetadataCleaner
 
   def cleanup_fields(solr_doc)
     FIELDS.each do |field|
-      solr_doc[field] = cleanup_field(field, solr_doc[field]) if solr_doc.key?(field)
+      solr_doc[field] = cleanup_field(solr_doc[field]) if solr_doc.key?(field)
     end
     solr_doc
   end
 
   # Pass in an array of values for a field and clean up each value
-  def cleanup_field(_field, values)
+  def cleanup_field(values)
+    values = [values] unless values.is_a?(Array)
     delim = quote_delimited(values)
     titleized = delim.map(&:titleize)
-    titleized.map { |val| val.delete_prefix('(').delete_suffix(')').strip }
+    trimmed = titleized.map { |val| val.delete_prefix('(').delete_suffix(')').strip }
+
+    values.is_a?(Array) ? trimmed : trimmed[0]
   end
 
   def quote_delimited(values)
