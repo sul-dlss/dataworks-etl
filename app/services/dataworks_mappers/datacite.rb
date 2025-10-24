66e82c06e5ac98108653dc3c83da61f46b56d5f4
Huda Khan <hudakhan@stanford.edu>
2025-10-24 05:03:49 -0600

    handling empty date

--- app/services/dataworks_mappers/datacite.rb
+++ app/services/dataworks_mappers/datacite.rb
@@ -90,6 +90,8 @@ module DataworksMappers
     end
 
     def cleanup_date(date)
+      return if date.blank?
+
       date.strip!
       date.sub!(' to ', '/')
       date << 'Z' if date.match?(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/)
