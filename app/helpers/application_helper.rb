6c0987a45e4794eb34d5752c88cb59866220736c
Nick Budak <budak@stanford.edu>
2025-10-27 12:13:38 -0700

    Rewrite MIME types to friendly names for format facet (#417)
    Ref #23

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,15 @@ module ApplicationHelper
     nil
   end
   # rubocop:enable Metrics/CyclomaticComplexity
+
+  # Given a string that may be a MIME type, return a more user-friendly name
+  # Checks translations first, then falls back to MIME::Types gem
+  # If no translation and no friendly name, return nil
+  def mime_type_friendly_name(mime_type)
+    I18n.t(
+      MIME::Type.i18n_key(mime_type),
+      default: MIME::Types[mime_type]&.first&.friendly,
+      scope: 'dataworks_etl.format'
+    )
+  end
 end
