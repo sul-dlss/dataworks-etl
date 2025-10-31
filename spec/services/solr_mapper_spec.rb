d03593a8e0a6e3c14a43bf06b0449c5a6030dd17
Huda Khan <hudakhan@stanford.edu>
2025-10-31 13:48:37 -0600

    adding comma

--- spec/services/solr_mapper_spec.rb
+++ spec/services/solr_mapper_spec.rb
@@ -67,7 +67,7 @@ RSpec.describe SolrMapper do
             geo_place_ssim: ['Vancouver, British Columbia, Canada', 'Victoria, British Columbia, Canada'],
             stanford_project_ssi: true,
             stanford_contributor_ssi: true,
-            stanford_dataset_ssi: true
+            stanford_dataset_ssi: true,
             access_contact_struct_ss: '[{"name":"Contact Person","email":"contact@contact.com"}]'
           }
         )
