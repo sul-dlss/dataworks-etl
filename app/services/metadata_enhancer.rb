21fa6a247bbd7034884220c5cd33f5b1f9294605
Huda Khan <hudakhan@stanford.edu>
2025-11-10 15:26:55 -0700

    Removing superfluous 7

--- app/services/metadata_enhancer.rb
+++ app/services/metadata_enhancer.rb
@@ -50,7 +50,7 @@ class MetadataEnhancer
   # If Stanford University does not exist as an affiliation, add that block
   def add_departments(contributor:, departments:)
     affiliation_info = {
-      name: 'Stanford University7',
+      name: 'Stanford University',
       affiliation_identifier: 'https://ror.org/00f54p054', affiliation_identifier_scheme: 'ROR',
       affiliation_department_name: departments
     }
