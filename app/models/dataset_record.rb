ccfeb92e8f7b529f72a15f18f6ad01e6b2ca9085
Nick Budak <budak@stanford.edu>
2025-11-25 15:57:02 -0800

    Don't print entire record source JSON in logs/inspect
    This causes some queries in a console session to take forever, because
    they have to prepare the entire record and then print it to the console.

--- app/models/dataset_record.rb
+++ app/models/dataset_record.rb
@@ -5,6 +5,10 @@ class DatasetRecord < ApplicationRecord
   has_many :dataset_record_associations, dependent: :destroy
   has_many :dataset_record_sets, through: :dataset_record_associations
 
+  # Don't bother printing the entire source JSON in logs/inspect, as it can
+  # slow things down considerably in a console session
+  self.filter_attributes = [:source]
+
   before_save ->(dataset_record) { dataset_record.source_md5 = Digest::MD5.hexdigest(dataset_record.source.to_json) }
 
   # @return [String] unique identifier for the dataset (independent of the provider)
