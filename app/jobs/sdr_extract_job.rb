db2463249bf3b40ba942cf79e0ba6cc64e5b8728
Nick Budak <budak@stanford.edu>
2025-11-25 16:27:46 -0800

    Don't use the job ID as load ID for SdrExtractJob

--- app/jobs/sdr_extract_job.rb
+++ app/jobs/sdr_extract_job.rb
@@ -6,10 +6,9 @@ class SdrExtractJob < ExtractJob
 
   def perform
     dataset_record_set = Extractors::Sdr.call
-    dataset_record_set.update!(job_id: @job_id) if @job_id
 
     Rails.logger.info "SdrExtractJob complete: DatasetRecordSet #{dataset_record_set.id} - " \
-                      "job #{dataset_record_set.job_id} - #{dataset_record_set.provider} - " \
+                      "job #{@job_id} - #{dataset_record_set.provider} - " \
                       "#{dataset_record_set.dataset_records.count} datasets"
   end
 end
