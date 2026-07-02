f03ffac3a9212f6f6c37868db8a8684c441dce50
Huda Khan <hudakhan@stanford.edu>
2026-07-02 11:48:15 -0600

    changing transform load to use batching

--- app/services/transformer_loader.rb
+++ app/services/transformer_loader.rb
@@ -32,13 +32,6 @@ class TransformerLoader
     end
   end
 
-  def grouped_dataset_records
-    dataset_records = DatasetRecord.joins(:dataset_record_associations)
-                                   .where(dataset_record_associations: { dataset_record_set: dataset_record_sets })
-
-    dataset_records.group_by(&:external_dataset_id)
-  end
-
   def solr
     @solr ||= SolrService.new
   end
@@ -54,14 +47,74 @@ class TransformerLoader
   # Return array of solr docs to be added to the index
   def transform_records
     solr_docs = []
-    grouped_dataset_records.each_value do |dataset_records|
-      solr_doc = DatasetTransformer.call(dataset_records:, load_id:, mapper_class:)
-      solr_docs << solr_doc if solr_doc
-    rescue DataworksMappers::MappingError
-      raise if fail_fast?
+
+    # We are going to work through batches of grouped datasets so we don't
+    # overwhelm memory
+    # dataset_ids is of the form of [[]], where each row is [doi:, provider:, id:]
+    list_dataset_ids.each_slice(100) do |dataset_ids|
+      # Now run transform on this set of grouped records
+      grouped_dataset_records(dataset_ids).each_value do |dataset_records|
+        solr_doc = DatasetTransformer.call(dataset_records:, load_id:, mapper_class:)
+        solr_docs << solr_doc if solr_doc
+      rescue DataworksMappers::MappingError
+        raise if fail_fast?
+      end
+
+      # Clear out memory
+      GC.start
     end
+
     # Remove previous or non-canonical versions
     consolidate_datasets(solr_docs)
+  end
+
+  # Create grouped records by id
+  def grouped_dataset_records(dataset_ids)
+    # First check by doi, then by provider and id combination
+    # DOIs are frequent but not always present. We use provider + id combination to uniquely
+    # identifiy a dataset. We have to account for both, especially as multiple providers
+    # may have the same DOI.
+    batch_records = (Array(lookup_dois(dataset_ids)) + Array(lookup_provider_id_combination(dataset_ids))).uniq(&:id)
+    # Group by "external dataset id" which is doi OR provider-id
+    batch_records.group_by(&:external_dataset_id)
+  end
+
+  # In order not to keep everything in memory as we process the records,
+  # we will first retrieve all the unique external ids we will group by
+  def list_dataset_ids
+    DatasetRecord.joins(:dataset_record_associations)
+                 .where(dataset_record_associations: { dataset_record_set: dataset_record_sets })
+                 .order(:doi)
+                 .pluck(:doi, :provider, :dataset_id)
+  end
+
+  # Look up all dois
+  # Input = { :doi, :provider, :dataset_id}
+  def lookup_dois(dataset_ids)
+    dois = dataset_ids.pluck(0).compact.uniq
+
+    if dois.any?
+      DatasetRecord.joins(:dataset_record_associations)
+                   .where(dataset_record_associations: { dataset_record_set: dataset_record_sets })
+                   .where(doi: dois)
+                   .includes(:dataset_record_associations)
+    else
+      []
+    end
+  end
+
+  def lookup_provider_id_combination(dataset_ids)
+    # Create pairs for each provider, id combination
+    provider_id_pairs = dataset_ids.map { |row| [row[1], row[2]] }.uniq
+
+    if provider_id_pairs.any?
+      DatasetRecord.joins(:dataset_record_associations)
+                   .where(dataset_record_associations: { dataset_record_set: dataset_record_sets })
+                   .where(%i[provider dataset_id] => provider_id_pairs)
+                   .includes(:dataset_record_associations)
+    else
+      []
+    end
   end
 
   # Given an array of solr docs, review which dois appear to be versions of the same
