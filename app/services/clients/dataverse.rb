9b0a5cff03035c9f5b49edcd8829ed1b72d28e88
Huda Khan <hudakhan@stanford.edu>
2026-08-07 23:53:28 -0600

    adding headers to test

--- app/services/clients/dataverse.rb
+++ app/services/clients/dataverse.rb
@@ -14,15 +14,17 @@ module Clients
     # Dataverse prefers we pass in the api token in the X-Dataverse-key header
     def new_conn
       Rails.logger.info('Dataverse token does not exist') if @dataverse_token.blank?
-      super
-      # base_conn.headers['X-Dataverse-key'] = @dataverse_token
+      base_conn = super
+      base_conn.headers['User-Agent'] = 'DataWorks Stanford Application - hudakhan@stanford.edu'
+      base_conn.headers['X-Dataverse-key'] = @dataverse_token
+      base_conn
     end
 
     # @param doi [String] the DOI of the dataset
     def dataset_doi(doi:)
       # params ?persistentId=doi:#{doi}
       get_json(path: '/api/datasets/:persistentId/',
-               params: { persistentId: "doi:#{doi}", key: @dataverse_token })
+               params: { persistentId: "doi:#{doi}" })
     end
   end
 end
